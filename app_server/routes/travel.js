1011618fa093e05b1620d95f2485a14cfdfec663
Sungho Yoon <tjdgh104@gmail.com>
2025-09-21 11:03:33 -0700

    Module 3 Completed Baseline

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/travel');
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
 
-/* GET travel page */
-router.get('/',controller.travel);
+/* GET travel view */
+const travel = (req, res) => {
+    res.render('travel', { title: 'Travlr Getaways', trips});
+};
 
-module.exports = router;
+module.exports = travel;
